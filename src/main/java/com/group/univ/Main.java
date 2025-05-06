184f2f8a2a16743675f90e63bece66744efe6735
FrancoisBardel <fb280601@gmail.com>
2025-05-06 10:35:20 +0200

    Base des controlleur

--- src/main/java/com/group/univ/Main.java
+++ src/main/java/com/group/univ/Main.java
@@ -1,5 +1,6 @@
 package com.group.univ;
 
+import com.group.univ.generator.PHPGeneratorController;
 import com.group.univ.generator.PHPGeneratorEntity;
 import com.group.univ.model.Entity;
 import com.group.univ.parser.XMLParser;
@@ -12,11 +13,13 @@ public class Main {
     public static void main(String[] args) {
         XMLParser xmlParser = new XMLParser();
         PHPGeneratorEntity phpGeneratorEntity = new PHPGeneratorEntity();
+        PHPGeneratorController phpGeneratorController = new PHPGeneratorController();
         try{
             Document document = xmlParser.loadXmlDocument("src/main/resources/com/group/univ/xml/schema.xml");
             Map<String, Entity> entities = xmlParser.parseEntities(document);
             xmlParser.parseRelations(document, entities);
             phpGeneratorEntity.generatePhpFiles(entities);
+            phpGeneratorController.generatePhpControllers(entities);
         }catch (Exception e){
             System.out.println("Erreur : " + e.getMessage());
         }
---
+++ src/main/resources/com/group/univ/php-template/controller.php.tpl
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class {{ENTITY_NAME}}Controller extends AbstractController
+{
+    /**
+     * @Route("/{{ENTITY_NAME_LOWER}}", name="{{ENTITY_NAME_LOWER}}_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('{{ENTITY_NAME_LOWER}}/index.html.twig', [
+        'controller_name' => '{{ENTITY_NAME}}Controller',
+        ]);
+    }
+}
